5386bdf5de04277a15727174b60a74fac4f03f8f
alexct-max <wow.kingofmoon@gmail.com>
2022-08-10 23:47:36 +0800

    更改初级筛选

--- img_primary_filter.py
+++ img_primary_filter.py
@@ -17,6 +17,11 @@ import shutil
 import numpy as np
 
 
+def mkdir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+
 # 检测输入图像是否需要
 def check_img(img_path):
     img = cv2.imread(img_path, flags=cv2.IMREAD_COLOR)
@@ -42,8 +47,7 @@ if __name__ == '__main__':
     root_dir = "../Image-Downloader-master/download_images/dog"
     file_suffix = "jpeg|jpg|png"
     remove_dir = root_dir + "/remove"
-    if not os.path.exists(remove_dir):
-        os.makedirs(remove_dir)
+    mkdir(remove_dir)
     for img_name in os.listdir(root_dir):
         # 对处理文件的类型进行过滤
         if re.search(file_suffix, img_name) is None:
